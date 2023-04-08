b5f648af5c8b59e342b25899266725b34684b2df
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-08 20:24:16 +0800

    1 file modified

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ const CANVAS_HEIGHT = canvas.height = 700;
 let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
-backgroundLayer1.src = '';
+backgroundLayer1.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-2.png'
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-3.png'
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-4.png'
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-5.png'
+
+
 
