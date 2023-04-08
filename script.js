e6c907d17534383a0f0bf357039118540bcdfbca
RareFonder <108814268+RareFonder@users.noreply.github.com>
2023-04-08 21:39:30 +0800

    1 file modified

--- script.js
+++ script.js
@@ -2,23 +2,50 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-1.png';
 const backgroundLayer2 = new Image();
-backgroundLayer2.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-2.png'
+backgroundLayer2.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-2.png';
 const backgroundLayer3 = new Image();
-backgroundLayer3.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-3.png'
+backgroundLayer3.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-3.png';
 const backgroundLayer4 = new Image();
-backgroundLayer4.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-4.png'
+backgroundLayer4.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-4.png';
 const backgroundLayer5 = new Image();
-backgroundLayer5.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-5.png'
+backgroundLayer5.src = 'https://stackblitz.com/files/web-platform-6guvtw/github/RareFonder/Parallax-Backgrounds/main/layer-5.png';
+
+class Layer {
+  constructor(image, speedModifier) {
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700;
+    this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
+  }
+  update() {
+    this.speed = gameSpeed * this.speedModifier;
+    if (this.x <= this.width) {
+      this.x = this.width + this.x - this.speed;
+    }
+    if (this.x2 <= this.width) {
+      this.x2 = this.width + this.x2 - this.speed;
+    }
+    this.x = Math.floor(this.x - this.speed);
+    this.x2 = Math.floor(this.x2 - this.speed);
+  }
+  draw() {
+
+  }
+}
 
 const animate = () => {
-  ctx.drawImage(backgroundLayer4, 0, 0); 
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   requestAnimationFrame(animate);
-}
+};
 animate();
 
 
